065a0bf1caf17f0cc020d69a8c321b259aae483d
Quentin Marcuzzi <quentin.marcuzzi@etu.unilim.fr>
2023-01-19 22:17:26 +0100

    First attempt to resize the canvas automatically

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,20 @@ export class Main {
      * @private
      */
     _init() {
+        // Defining some properties
+
+        this.canvas = document.querySelector('#mainBoard'); // The canvas element
+        // Trying to get the context of the canvas
+        try {
+            this.ctx = this.canvas.getContext('2d');
+        } catch (e) {
+            throw e; // In case of error, we throw it
+        }
+
+        // Initializing the events
         this._initEvents();
+        // Resizing the canvas
+        this._resizeCanvas();
     }
 
     /**
@@ -24,8 +37,26 @@ export class Main {
      * @private
      */
     _initEvents() {
+        // Content loaded event
         document.addEventListener('DOMContentLoaded', () => {
             console.log('%c Welcome to my mini-game!', 'font-size: 36px;');
         });
+
+        // Window resize event
+        window.addEventListener('resize', () => {
+            this._resizeCanvas();
+        });
+    }
+
+    /**
+     * This method is used to resize the canvas with a proper ratio.
+     * This should preserve a good quality of the game textures.
+     * @private
+     */
+    _resizeCanvas() {
+        let ratio = window.devicePixelRatio; // The ratio of the device
+        this.canvas.width = document.querySelector('main').clientWidth * ratio; // The width of the canvas
+        this.canvas.height = document.querySelector('main').clientHeight * ratio; // The height of the canvas
+        this.ctx.scale(ratio, ratio); // Scaling the context
     }
 }
